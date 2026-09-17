6f6ebc9b1ec4467ccab05531f669124a923c385b
IgorPuts <exNeron@yandex.ru>
2026-09-17 13:20:31 +0300

    feat: add carts.js data file

---
+++ js/carts.js
@@ -0,0 +1,182 @@
+export const menuData = [
+  {
+    category: "coffee",
+    name: "Irish coffee",
+    description: "Fragrant black coffee with Jameson Irish whiskey and whipped milk",
+    price: 7.00,
+    currency: "$",
+    image: "../assets/img/coffee-1.png",
+    alt: "Irish coffee in a glass with whipped milk"
+  },
+  {
+    category: "coffee",
+    name: "Kahlua coffee",
+    description: "Classic coffee with milk and Kahlua liqueur under a cap of frothed milk",
+    price: 7.00,
+    currency: "$",
+    image: "../assets/img/coffee-2.png",
+    alt: "Kahlua coffee in a tall glass with frothed milk"
+  },
+  {
+    category: "coffee",
+    name: "Honey raf",
+    description: "Espresso with frothed milk, cream and aromatic honey",
+    price: 5.50,
+    currency: "$",
+    image: "../assets/img/coffee-3.png",
+    alt: "Honey raf coffee with cream and honey"
+  },
+  {
+    category: "coffee",
+    name: "Ice cappuccino",
+    description: "Cappuccino with soft thick foam in summer version with ice",
+    price: 5.00,
+    currency: "$",
+    image: "../assets/img/coffee-4.png",
+    alt: "Ice cappuccino with thick foam"
+  },
+  {
+    category: "coffee",
+    name: "Espresso",
+    description: "Classic black coffee",
+    price: 4.50,
+    currency: "$",
+    image: "../assets/img/coffee-5.png",
+    alt: "Classic black espresso in a glass cup"
+  },
+  {
+    category: "coffee",
+    name: "Latte",
+    description: "Espresso coffee with the addition of steamed milk and dense milk foam",
+    price: 5.50,
+    currency: "$",
+    image: "../assets/img/coffee-6.png",
+    alt: "Latte with steamed milk and foam"
+  },
+  {
+    category: "coffee",
+    name: "Latte macchiato",
+    description: "Espresso with frothed milk and chocolate",
+    price: 5.50,
+    currency: "$",
+    image: "../assets/img/coffee-7.png",
+    alt: "Latte macchiato with chocolate"
+  },
+  {
+    category: "coffee",
+    name: "Coffee with cognac",
+    description: "Fragrant black coffee with cognac and whipped cream",
+    price: 6.50,
+    currency: "$",
+    image: "../assets/img/coffee-8.png",
+    alt: "Coffee with cognac and whipped cream"
+  },
+  {
+    category: "tea",
+    name: "Moroccan",
+    description: "Fragrant black tea with the addition of tangerine, cinnamon, honey, lemon and mint",
+    price: 4.50,
+    currency: "$",
+    image: "../assets/img/tea-1.png",
+    alt: "Moroccan black tea with tangerine and cinnamon"
+  },
+  {
+    category: "tea",
+    name: "Ginger",
+    description: "Original black tea with fresh ginger, lemon and honey",
+    price: 5.00,
+    currency: "$",
+    image: "../assets/img/tea-2.png",
+    alt: "Ginger black tea with lemon and honey"
+  },
+  {
+    category: "tea",
+    name: "Cranberry",
+    description: "Invigorating black tea with cranberry and honey",
+    price: 5.00,
+    currency: "$",
+    image: "../assets/img/tea-3.png",
+    alt: "Cranberry black tea with honey"
+  },
+  {
+    category: "tea",
+    name: "Sea buckthorn",
+    description: "Toning sweet black tea with sea buckthorn, fresh thyme and cinnamon",
+    price: 5.50,
+    currency: "$",
+    image: "../assets/img/tea-4.png",
+    alt: "Sea buckthorn black tea with thyme and cinnamon"
+  },
+  {
+    category: "dessert",
+    name: "Marble cheesecake",
+    description: "Philadelphia cheese with lemon zest on a light sponge cake and red currant jam",
+    price: 3.50,
+    currency: "$",
+    image: "../assets/img/dessert-1.png",
+    alt: "Marble cheesecake with lemon zest and red currant jam"
+  },
+  {
+    category: "dessert",
+    name: "Red velvet",
+    description: "Layer cake with cream cheese frosting",
+    price: 4.00,
+    currency: "$",
+    image: "../assets/img/dessert-2.png",
+    alt: "Red velvet layer cake with cream cheese frosting"
+  },
+  {
+    category: "dessert",
+    name: "Cheesecakes",
+    description: "Soft cottage cheese pancakes with sour cream and fresh berries and sprinkled with powdered sugar",
+    price: 4.50,
+    currency: "$",
+    image: "../assets/img/dessert-3.png",
+    alt: "Cottage cheese pancakes with berries and powdered sugar"
+  },
+  {
+    category: "dessert",
+    name: "Creme brulee",
+    description: "Delicate creamy dessert in a caramel basket with wild berries",
+    price: 4.00,
+    currency: "$",
+    image: "../assets/img/dessert-4.png",
+    alt: "Creme brulee in a caramel basket with wild berries"
+  },
+  {
+    category: "dessert",
+    name: "Pancakes",
+    description: "Tender pancakes with strawberry jam and fresh strawberries",
+    price: 4.50,
+    currency: "$",
+    image: "../assets/img/dessert-5.png",
+    alt: "Tender pancakes with strawberry jam and fresh strawberries"
+  },
+  {
+    category: "dessert",
+    name: "Honey cake",
+    description: "Classic honey cake with delicate custard",
+    price: 4.50,
+    currency: "$",
+    image: "../assets/img/dessert-6.png",
+    alt: "Classic honey cake with delicate custard"
+  },
+  {
+    category: "dessert",
+    name: "Chocolate cake",
+    description: "Cake with hot chocolate filling and nuts with dried apricots",
+    price: 5.50,
+    currency: "$",
+    image: "../assets/img/dessert-7.png",
+    alt: "Chocolate cake with hot chocolate filling and nuts"
+  },
+  {
+    category: "dessert",
+    name: "Black forest",
+    description: "A combination of thin sponge cake with cherry jam and light chocolate mousse",
+    price: 6.50,
+    currency: "$",
+    image: "../assets/img/dessert-8.png",
+    alt: "Black forest cake with cherry jam and chocolate mousse"
+  }
+];
